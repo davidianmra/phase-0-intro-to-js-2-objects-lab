8c53c47ad4bbdb181b711109ef9fefd58e40e594
David Rashid <davidmra@umich.edu>
2023-02-11 19:37:56 -0500

    commit tree 0d83724f0718a02116667198a494f50f646e21e0

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+const employee = {
+    name: {},
+    streetAddress: {},
+
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+const updatedEmployee = {...employee};
+updatedEmployee[key] = value;
+return updatedEmployee;
+};
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+
+};
+
+function deleteFromEmployeeByKey(employee, key){
+    const cloneEmployee = {...employee};
+    delete cloneEmployee[key];
+    return cloneEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+};
